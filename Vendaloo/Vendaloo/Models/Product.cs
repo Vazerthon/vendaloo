5d758f56f148d71296b05ea5526ab12417d411da
Vallis <paul@mrvallis.co.uk>
2016-04-26 19:30:39 +0100

    Added product model

---
+++ Vendaloo/Vendaloo/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Vendaloo.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
