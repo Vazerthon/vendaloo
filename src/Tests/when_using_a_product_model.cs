c616d24271a6f549eb9f682402303225014a63f4
Paul Vallis <paul.vallis@connect.auto>
2017-01-20 13:13:30 +0000

    uncommented tests

--- src/Tests/when_using_a_product_model.cs
+++ src/Tests/when_using_a_product_model.cs
@@ -1,108 +1,99 @@
-﻿// using NUnit.Framework;
-// using Vendaloo.Models;
+﻿using NUnit.Framework;
+using Vendaloo.Models;
 
-// namespace Vendaloo.Tests
-// {
-//     [TestFixture]
-//     public class when_using_a_product_model
-//     {
-//         [Test]
-//         public void it_should_set_the_id_correctly()
-//         {
-//             // Arrange
+namespace Vendaloo.Tests
+{
+    [TestFixture]
+    public class when_using_a_product_model
+    {
+        [Test]
+        public void it_should_set_the_id_correctly()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
+            // Assert
+        }
 
-//         [Test]
-//         public void it_should_set_the_name_correctly()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_set_the_name_correctly()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }   
+            // Assert
+        }   
 
-//         [Test]
-//         public void it_should_set_the_price_correctly()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_set_the_price_correctly()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
+            // Assert
+        }
 
-//         [Test]
-//         public void it_should_set_the_stock_correctly()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_set_the_stock_correctly()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
+            // Assert
+        }
 
-//         [Test]
-//         public void it_should_throw_an_exception_if_the_name_is_null_or_whitespace()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_throw_an_exception_if_the_name_is_null_or_whitespace()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
+            // Assert
+        }
 
-//         [Test]
-//         public void it_should_throw_an_exception_if_the_price_is_less_than_or_equal_to_zero()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_throw_an_exception_if_the_price_is_less_than_or_equal_to_zero()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
+            // Assert
+        }
 
-//         [Test]
-//         public void it_should_throw_an_exception_if_the_stock_is_less_than_zero()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_throw_an_exception_if_the_stock_is_less_than_zero()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
+            // Assert
+        }
 
-//         [Test]
-//         public void it_should_reduce_the_stock_by_x_when_calling_ReduceStockBy()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_reduce_the_stock_by_x_when_calling_ReduceStockBy()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
+            // Assert
+        }
 
-//         [Test]
-//         public void it_should_set_the_stock_to_zero_when_calling_ReduceStockBy_with_a_number_larger_than_the_stock_value()
-//         {
-//             // Arrange
+        [Test]
+        public void it_should_set_the_stock_to_zero_when_calling_ReduceStockBy_with_a_number_larger_than_the_stock_value()
+        {
+            // Arrange
 
-//             // Act
+            // Act
 
-//             // Assert
-//             Assert.IsTrue(false);
-//         }
-//     }
-// }
+            // Assert
+        }
+    }
+}
